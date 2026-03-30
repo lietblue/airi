de79fedb926ddd0aa4c93f1101f7018ed9c86315
Doji <2212887241@qq.com>
2026-03-30 11:29:19 +0800

    feat(model-driver-mediapipe): fix missing model file

--- packages/model-driver-mediapipe/tasks/prepare-tasks.ts
+++ packages/model-driver-mediapipe/tasks/prepare-tasks.ts
@@ -18,6 +18,7 @@ const taskSources: Record<keyof VisionTaskAssets, string> = {
   pose: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
   hands: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
   face: 'https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task',
+  faceDetector: 'https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite',
 }
 
 const assetsRoot = fileURLToPath(new URL('./assets', import.meta.url))
